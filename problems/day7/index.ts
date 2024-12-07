42311e944740a8bb2b3db078abe4362deabcea32
Giovanni Gardusi <giovanni.gardusi@gmail.com>
2024-12-07 17:45:32 -0300

    Day 7 solution

---
+++ problems/day7/index.ts
@@ -0,0 +1,71 @@
+import { readFileSync } from "fs"
+
+type Equation = {
+    result: number
+    numbers: number[]
+}
+
+type Operation = (a: number, b: number) => number
+
+type SumNode = {
+    sum?: number
+    children?: SumNode[]
+}
+
+const equations: Equation[] = readFileSync('./problems/day7/input.txt', 'utf8')
+    .replaceAll('\r', '')
+    .split('\n')
+    .map(line => {
+        const [result, ...numbers] = line.split(' ')
+        return {
+            result: Number(result.slice(0, -1)),
+            numbers: numbers.map(Number)
+        }
+    })
+
+const part1Operations: Operation[] = [
+    (a, b) => a + b, // sum
+    (a, b) => a * b, // product
+]
+
+const part2Operations: Operation[] = [
+    (a, b) => a + b, // sum
+    (a, b) => a * b, // product
+    (a, b) => Number(`${a}${b}`), // concatenation
+]
+
+const splitSum = (sumNode: SumNode, equation: Equation, index: number, sums: number[], operations: Operation[]) => {
+    const sum = sumNode.sum || 0
+    delete sumNode.sum
+
+    const children = operations.map(operation => ({ sum: operation(sum, equation.numbers[index]) }))
+    sumNode.children = children
+
+    if (index < equation.numbers.length - 1) {
+        for (const child of sumNode.children) {
+            splitSum(child, equation, index + 1, sums, operations)
+        }
+        return
+    }
+    sums.push(...children.map(child => child.sum))
+}
+
+const runEquations = (operations: Operation[]) => {
+    let totalSum: number = 0
+    for (const equation of equations) {
+        const sumTree: SumNode = {
+            sum: equation.numbers[0]
+        }
+
+        const sums: number[] = []
+        splitSum(sumTree, equation, 1, sums, operations)
+
+        if (sums.includes(equation.result)) {
+            totalSum += equation.result
+        }
+    }
+    return totalSum
+}
+
+console.log('Part 1 Sum', runEquations(part1Operations))
+console.log('Part 2 Sum', runEquations(part2Operations))
