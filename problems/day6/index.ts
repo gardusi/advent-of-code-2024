7f7b592cc9e26e2c4d147e36d8528d734a60151c
Giovanni Gardusi <giovanni.gardusi@gmail.com>
2024-12-07 21:05:52 -0300

    Day 6 solution (brute-force-ish)

--- problems/day6/index.ts
+++ problems/day6/index.ts
@@ -9,23 +9,25 @@ type Guard = {
     direction: Direction
 }
 
-type Tile = {
-    directions: Direction[],
-    corner: string[],
-    obstacle: number
-}
-
-const grid = readFileSync('./problems/day6/input.txt', 'utf8')
+const originalGrid = readFileSync('./problems/day6/input.txt', 'utf8')
     .replaceAll('\r', '')
     .split('\n')
     .map(line => line.split(''))
 
-const mX = grid.length
-const mY = grid[0].length
+const mX = originalGrid.length
+const mY = originalGrid[0].length
 
-let guard: Guard = {
+let originalGuard: Guard = {
     position: [0, 0],
     direction: '^'
+}
+
+for (let i = 0; i < mX; i++) {
+    for (let j = 0; j < mY; j++) {
+        if (originalGrid[i][j] === originalGuard.direction) {
+            originalGuard.position = [i, j]
+        }
+    }
 }
 
 const ahead = ({ position, direction }: Guard) => {
@@ -52,42 +54,7 @@ const turn = ({ direction }: Guard) => {
     return rotation[direction]
 }
 
-const entireColumn = (path: Tile[][], y: number) => {
-    return path.map(column => column[y])
-}
-
-const entireRow = (path: Tile[][], x: number) => {
-    return path[x]
-}
-
-const alignedWithPreviousPath = (guard: Guard, tile: Tile) => {
-    const rotation: Record<Direction, Direction[]> = {
-        '<': ['^'],
-        '^': ['>'],
-        '>': ['v'],
-        'v': ['<'],
-    }
-
-    return rotation[guard.direction]
-        .some((direction) => tile.directions.includes(direction))
-}
-
-const canLoop = (guard: Guard, path: Tile[][]) => {
-    if (guard.direction === '<' || guard.direction === '>') {
-        const column = entireColumn(path, guard.position[1]).filter(tile => tile.directions.length > 0)
-
-        // console.log(column.map(tile => tile.directions))
-
-        return column.some((tile) => alignedWithPreviousPath(guard, tile))
-    }
-    const row = entireRow(path, guard.position[0]).filter(tile => tile.directions.length > 0)
-
-    // console.log(row.map(tile => tile.directions))
-
-    return row.some((tile) => alignedWithPreviousPath(guard, tile))
-}
-
-const move = (guard: Guard, grid: string[][], path: Tile[][]) => {
+const move = (guard: Guard, grid: string[][]) => {
     const [nx, ny] = ahead(guard)
 
     if (nx < 0 || nx >= mX || ny < 0 || ny >= mY) {
@@ -96,82 +63,57 @@ const move = (guard: Guard, grid: string[][], path: Tile[][]) => {
 
     switch (grid[nx][ny]) {
         case '#':
-            // path[guard.position[0]][guard.position[1]].directions.push(guard.direction)
             guard.direction = turn(guard)
-            path[guard.position[0]][guard.position[1]].directions.push(guard.direction)
-
-            if (canLoop(guard, path)) {
-                const [ox, oy] = ahead(guard)
-                path[ox][oy].obstacle += 1
-            }
-            
             return true
         default:
             guard.position = [nx, ny]
-            if (canLoop(guard, path)) {
-                const [ox, oy] = ahead(guard)
-                path[ox][oy].obstacle += 1
-            }
-            path[nx][ny].directions.push(guard.direction)
             return true
     }
 }
 
-for (let i = 0; i < mX; i++) {
-    for (let j = 0; j < mY; j++) {
-        if (grid[i][j] === guard.direction) {
-            guard.position = [i, j]
-        }
-    }
+const copyMatrix = <T>(grid: T[][]) => {
+    return [...grid.map(row => [...row])]
 }
 
-const path = Array.from(
-    { length: mX }, () => Array.from(
-        { length: mY }, () => ({
-            directions: [],
-            corner: [],
-            obstacle: 0
-        } as Tile)
-    )
-)
+const previousPath = Array.from({ length: mX }, () => Array.from({ length: mY }, () => false))
+const infiniteLoopMatrix = Array.from({ length: mX }, () => Array.from({ length: mY }, () => false))
 
-path[guard.position[0]][guard.position[1]] = {
-    directions: ['^'],
-    corner: [],
-    obstacle: 0
-}
-
-const main = async (debug: boolean) => {
-    let count = 0;
+const main = async (copy: boolean, grid: string[][], guard: Guard) => {
+    let steps = 0
     do {
-        if (debug) {
-            console.log(path.map((row) => row.map((tile) => tile.directions.length === 1 ? tile.directions[0] : tile.directions.length > 1 ? '*' : '.').join('')).join('\n'))
-
-            console.log('')
-            console.log('-'.repeat(mY))
-            console.log('')
-
-            console.log(path.map((row, i) => row.map((tile, j) => guard.position[0] === i && guard.position[1] === j ? guard.direction : tile.obstacle > 0 ? 'O' : tile.directions.length > 0 ? 'X' : '.').join('')).join('\n'))
-            console.log('Count:', count)
-            await new Promise(resolve => process.stdin.once('data', resolve))
-            
-            // await new Promise(resolve => setTimeout(resolve, 1))
-            // console.clear()
-            count++
+        if (copy) {
+            previousPath[guard.position[0]][guard.position[1]] = true
         }
-    } while (move(guard, grid, path))
-
-    let pathCount = 0
-    let obstacleCount = 0
-    for (let i = 0; i < mX; i++) {
-        for (let j = 0; j < mY; j++) {
-            pathCount += path[i][j].directions.length > 0 ? 1 : 0
-            obstacleCount += path[i][j].obstacle
+        if (steps > 10000) {
+            return { steps: Infinity }
         }
-    }
+        steps++
+        if (!copy) {
+            continue
+        }
+        const gridCopy = copyMatrix(grid)
+        const [ox, oy] = ahead(guard)
+        if (ox < 0 || ox >= mX || oy < 0 || oy >= mY) {
+            continue
+        }
+        if (previousPath[ox][oy]) {
+            continue
+        }
+        gridCopy[ox][oy] = '#'
 
-    console.log('Path taken:', pathCount)
-    console.log('Obstacles:', obstacleCount)
+        console.log('Creating new simulation for step', steps)
+        const simulation = await main(false, gridCopy, { ...guard })
+
+        if (simulation.steps > 10000) {
+            console.log('Infinite loop detected')
+            infiniteLoopMatrix[ox][oy] = true
+        }          
+    } while (move(guard, grid))
+
+    return { steps }
 }
 
-main(false)
+main(true, originalGrid, originalGuard).then(({ steps }) => {
+    console.log('Infinite loops detected:', infiniteLoopMatrix.flat().filter(Boolean).length)
+    console.log('Algorithm steps:', steps)
+})
